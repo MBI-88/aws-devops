9e8518cef25d8c26997bfcf9e9a7733bc4802979
MBI <59748306+MBI-88@users.noreply.github.com>
2024-07-07 19:50:23 -0300

    test server done

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express'
+import swig from 'swig'
+
+const app = express()
+const port = 80
+
+app.engine('html', swig.renderFile)
+app.set('view engine', 'html')
+app.set('views',__dirname + "/views")
+
+
+app.get("/", (req: Request, res: Response) => {
+    res.render('index',{ip: req.ip})
+})
+
+
+app.listen(port, () => {
+    console.log(`Server running on 0.0.0.0:${port} `)
+})
